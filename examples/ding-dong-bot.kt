1691054f9ca57984cd5b39fc07cb6dc3c71a3312
Zhengxin <diaozxin@163.com>
2020-05-22 10:19:36 +0800

    add index of java and kt

---
+++ examples/ding-dong-bot.kt
@@ -0,0 +1,4 @@
+/**
+ * This file is a helper file to point to the DingDongBot.kt source code at:
+ *  src/main/kotlin/io/github/wechaty/DingDongBot.kt
+ */
---
+++ examples/dong-dong-bot.java
@@ -0,0 +1,4 @@
+/**
+ * This file is a helper file to point to the DingDongBot.java source code at:
+ *  src/main/java/io/github/wechaty/DingDongBot.java
+ */
